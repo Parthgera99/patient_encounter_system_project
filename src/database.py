316d95c1b189862d1b6f52d71f5dbfbe680a3b0b
Parth Gera <parthgera2004@gmail.com>
2026-02-04 17:09:01 +0530

    db change again

--- src/database.py
+++ src/database.py
@@ -1,9 +1,13 @@
+# import os
+
 # from sqlalchemy import create_engine
 # from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-# # SQLite for all environments
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# DB_PATH = os.path.join(BASE_DIR, "app.db")
+
 # engine = create_engine(
-#     "sqlite:///./app.db",
+#     f"sqlite:///{DB_PATH}",
 #     connect_args={"check_same_thread": False},
 #     echo=False,
 # )
@@ -23,17 +27,14 @@
 #     Base.metadata.create_all(bind=engine)
 
 
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "app.db")
+from sqlalchemy.pool import StaticPool
 
 engine = create_engine(
-    f"sqlite:///{DB_PATH}",
+    "sqlite://",
     connect_args={"check_same_thread": False},
+    poolclass=StaticPool,  # 🔥 THIS IS CRITICAL
     echo=False,
 )
 
